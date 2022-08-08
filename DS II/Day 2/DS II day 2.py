493594757ead9874ad810e2432fa0788d2b61f00
Tushar Patel <tushar.1923en1113@kiet.edu>
2022-08-08 13:11:54 -0800

    Create DS II day 2.py

---
+++ DS II/Day 2/DS II day 2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        right = len(nums)-1
+        mid = (left+right+1)//2
+        i=0
+        while  i <= right:
+            if nums[i] == 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left+=1
+            elif nums[i] == 2:
+                nums[right], nums[i] = nums[i], nums[right]
+                right-=1
+                i-=1
+            i+=1
